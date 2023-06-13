e50af8c8ea5e801b469ca0c828150825c1fd9be7
yenilikci <m.melihcelik017@outlook.com>
2023-06-13 18:54:43 +0300

    creating the ViewModel and displaying the result

---
+++ BMICalculator/MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BMICalculator.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator.MVVM.ViewModels
+{
+    public class BMIViewModel
+    {
+        public BMI BMI { get; set; }
+
+        public BMIViewModel()
+        {
+            BMI = new BMI();
+            BMI.Weight = 73;
+            BMI.Height = 180;
+        }
+    }
+}
